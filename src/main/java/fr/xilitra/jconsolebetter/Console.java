915b986f3b453d19b1cde153fda9d4955dabb4fb
XiliTra <philippenelson59@gmail.com>
2023-01-15 22:50:08 +0100

    initial commit

--- src/main/java/fr/xilitra/jconsolebetter/Console.java
+++ src/main/java/fr/xilitra/jconsolebetter/Console.java
@@ -1,2 +1,13 @@
-package fr.xilitra.jconsolebetter;public class Console {
+package fr.xilitra.jconsolebetter;
+
+public class Console {
+
+    private String lastLine = "";
+    private String lineInput = "";
+    private int line_position = 0;
+
+    public void write(){
+
+    }
+
 }
--- src/main/java/fr/xilitra/jconsolebetter/Test.java
+++ src/main/java/fr/xilitra/jconsolebetter/Test.java
@@ -1,4 +1,21 @@
-package main.java.fr.xilitra.jconsolebetter;
+package fr.xilitra.jconsolebetter;
 
-public class Main {
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        new Thread(() -> {
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Hello World!");
+        }).start();
+    }
+
 }
